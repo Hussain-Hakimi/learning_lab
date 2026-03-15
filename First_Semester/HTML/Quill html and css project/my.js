a4c5e8aa51f87a969da9a56c24d96efcec1d3aa7
Hussain Hakimi <hussain.hakimi216@gmail.com>
2026-03-15 17:23:47 +0500

    I have done my first project about the html and css

---
+++ First_Semester/HTML/Quill html and css project/my.js
@@ -0,0 +1,16 @@
+console.log("Hello world");
+console.log("This is a test file.");
+console.log("This file is used to demonstrate code completion.");
+console.log("The code completion should work for this file.");
+console.log("The code completion should also work for this file.");
+console.log("The code completion should work for this file as well.");
+console.log("The code completion should work for this file too.");
+console.log("The code completion should work for this file also.");
+/* This is a multi-line comment. */
+
+/* this is my first js file. */
+/* I am learning JavaScript. 
+I am enjoying learning JavaScript. 
+JavaScript is a great language.
+I am excited to learn more about JavaScript. 
+I am looking forward to learning more about JavaScript. */
