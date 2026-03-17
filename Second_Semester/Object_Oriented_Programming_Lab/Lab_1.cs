78945a548b9837f18a56f4cbebb489017782c186
Hussain Hakimi <hussain.hakimi216@gmail.com>
2026-03-18 00:50:11 +0500

    Initial commit: setup C# .NET Framework console app
    Added project files for "Object_Oriented_Programming_Lab" targeting .NET Framework 4.8. Included initial source code (Lab_1.cs) with a simple Main method, configuration files, assembly metadata, solution and editor layout files, and build outputs. This establishes the base structure for the project.

---
+++ Second_Semester/Object_Oriented_Programming_Lab/Lab_1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Object_Oriented_Programming_Lab
+{
+    internal class Lab_1
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("This is my first lab in Object Oriented Programming"  );
+
+        }
+
+    }
+}
