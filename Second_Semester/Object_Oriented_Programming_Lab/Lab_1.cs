93042aa46a6e76cec3056e484792b7d2be012565
Hussain Hakimi <hussain.hakimi216@gmail.com>
2026-03-22 23:55:21 +0500

    Refactor code structure and clean up unused usings
    Simplified Main methods, removed unnecessary using directives, and improved formatting in Lab_1.cs and Program.cs. Added a placeholder method FirstSimpleExercise(). Deleted obsolete .vsidx files and updated binaries after recompilation.

--- Second_Semester/Object_Oriented_Programming_Lab/Lab_1.cs
+++ Second_Semester/Object_Oriented_Programming_Lab/Lab_1.cs
@@ -1,18 +1,22 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Security.Cryptography.X509Certificates;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Object_Oriented_Programming_Lab
 {
     internal class Lab_1
     {
-        static void Main(string[] args)
+        static void Main()
         {
             Console.WriteLine("Hello World!");
-            Console.WriteLine("This is my first lab in Object Oriented Programming"  );
+            Console.WriteLine("This is my first lab in Object Oriented Programming");
+
+
+
+
+
+        }
+
+        public static void FirstSimpleExercise()
+        {
 
         }
 
--- Second_Semester/Object_Oriented_Programming_Lab/Program.cs
+++ Second_Semester/Object_Oriented_Programming_Lab/Program.cs
@@ -1,17 +1,10 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Object_Oriented_Programming_Lab
+public class program
 {
-    internal class Program
+    public static void MYclass()
     {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
-            Console.WriteLine("This is my first lab in Object Oriented Programming");
-        }
+        Console.WriteLine("Hello World");
     }
+
+
 }
