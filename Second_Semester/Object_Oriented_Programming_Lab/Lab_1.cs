6d77604761f02a8bd09abd28554aaed9c2a29459
Hussain Hakimi <hussain.hakimi216@gmail.com>
2026-03-24 21:32:46 +0500

    Refactor: Update Lab_1.cs to streamline methods and improve readability

--- Second_Semester/Object_Oriented_Programming_Lab/Lab_1.cs
+++ Second_Semester/Object_Oriented_Programming_Lab/Lab_1.cs
@@ -1,28 +1,45 @@
 ﻿using System;
-
-namespace Object_Oriented_Programming_Lab
+class Lab_1
 {
-    internal class Lab_1
+    public static void Main(string[] args)
     {
-        static void Main()
-        {
-            Console.WriteLine("Hello World!");
-            Console.WriteLine("This is my first lab in Object Oriented Programming");
+        First10problems();
+    }
+    static void First10problems()
+
+    {   // This program swaps two numbers entered by the user
+        int num1, num2, t;
+        Console.WriteLine("Enter the first number:");
+        num1 = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Enter the second number:");
+        num2 = Convert.ToInt32(Console.ReadLine());
+
+        t = num1;
+        num1 = num2;
+        num2 = t;
+
+        Console.WriteLine("After swapping:");
+        Console.WriteLine("First number: " + num1);
+        Console.WriteLine("Second number: " + num2);
+
+
+        // Multiply Three Numbers
+        int num3, result;
+        Console.Write("Input the first number to multiply: ");
+        num1 = Convert.ToInt32(Console.ReadLine());
+
+        Console.Write("Input the second number to multiply: ");
+        num2 = Convert.ToInt32(Console.ReadLine());
+
+        Console.Write("Input the third number to multiply: ");
+        num3 = Convert.ToInt32(Console.ReadLine());
+
+        result = num1 * num2 * num3;
+
+        Console.WriteLine($"{num1} x {num2} x {num3} = {result}");
 
 
 
-
-
-        }
-
-        public static void FirstSimpleExercise()
-        {
-            //Print Hello and Name
-
-
-
-
-        }
 
     }
 }
