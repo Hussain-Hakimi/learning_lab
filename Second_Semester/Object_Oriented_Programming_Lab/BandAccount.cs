c6f448fdd22e300e6fab3594562df9d00dba92e7
Hussain Hakimi <hussain.hakimi216@gmail.com>
2026-03-30 14:03:23 +0500

    i have made BankAccount class that have setter and getter and fully functional

---
+++ Second_Semester/Object_Oriented_Programming_Lab/BandAccount.cs
@@ -0,0 +1,35 @@
+﻿internal class BankAccount
+{
+    public int AccountNumber { get; }           // Read‑only after construction
+    public decimal Balance { get; private set; } // Can be changed only within the class
+
+    // Constructor that sets account number and initial balance
+    public BankAccount(int accountNumber, decimal initialBalance)
+    {
+        AccountNumber = accountNumber;
+        Balance = initialBalance;
+    }
+
+    // Constructor that sets account number and defaults balance to 0
+    public BankAccount(int accountNumber) : this(accountNumber, 0)
+    {
+    }
+
+    // Optional: methods to modify balance
+    public void Deposit(decimal amount)
+    {
+        if (amount > 0)
+            Balance += amount;
+    }
+
+    public bool Withdraw(decimal amount)
+    {
+        if (amount > 0 && Balance >= amount)
+        {
+            Balance -= amount;
+            return true;
+        }
+        return false;
+    }
+}
+
