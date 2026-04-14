7f9f4b41d448027f37ddc41ed1ac8d66b424123e
Hussain Hakimi <hussain.hakimi216@gmail.com>
2026-04-14 13:35:50 +0500

    make Book Class with basic property

---
+++ Second_Semester/Object_Oriented_Programming_Lab/Book.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+class Book
+{
+    public string Title { get; set; }
+    public string Author { get; set; }
+
+    private bool isBorrowed;
+
+    public Book(string title, string author)
+    {
+        Title = title;
+        Author = author;
+    }
+
+    public void BorrowBook()
+    {
+        if (!isBorrowed)
+        {
+            isBorrowed = true;
+            Console.WriteLine($"Borrowed: {Title}");
+        }
+        else
+        {
+            Console.WriteLine($"Already borrowed: {Title}");
+        }
+    }
+
+    public void ReturnBook()
+    {
+        if (isBorrowed)
+        {
+            isBorrowed = false;
+            Console.WriteLine($"Returned: {Title}");
+        }
+        else
+        {
+            Console.WriteLine("Book was not borrowed");
+        }
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        Book b1 = new Book("Book A", "Author A");
+
+        b1.BorrowBook(); // success
+        b1.BorrowBook(); // fail
+        b1.ReturnBook(); // success
+        b1.ReturnBook(); // fail
+    }
+}
